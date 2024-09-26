47b94b5a037e08762fe604296e53e1a3c6722b1e
Whitos <pavel.tsanev@lycee-faure.fr>
2024-09-26 10:37:47 +0200

    feat(Piece): Création de la classe

---
+++ src/Piece.java
@@ -0,0 +1,25 @@
+public abstract class Piece {
+    private TypePiece typePiece;
+    private String niveau;
+
+    
+    public Piece(TypePiece typePiece, String niveau) {
+        this.typePiece = typePiece;
+        this.niveau = niveau;
+    }
+
+    public TypePiece getTypePiece() {
+        return typePiece;
+    }
+
+    public String getNiveau() {
+        return niveau;
+    }
+
+    public abstract double surface();
+
+    @Override
+    public String toString() {
+        return typePiece + " surface : " + surface() + "m² \n";
+    }
+}
